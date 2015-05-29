bd3f28db4f9fa89664b26185730f2a4198ced35b
Benke Tibor <ihrwein@gmail.com>
2015-05-29 16:51:09 +0200

    trie[wip]: implement insert()

--- src/matcher/trie/trie.rs
+++ src/matcher/trie/trie.rs
@@ -1,5 +1,28 @@
-use matcher::trie::node::Node;
+use matcher::trie::node::{Node, NodeType};
+use matcher::trie::node::CompiledPattern;
 
 pub struct PatternTrie<'a, 'b> {
     root: Node<'a, 'b>
+}
+
+impl <'a, 'b> PatternTrie<'a, 'b> {
+    pub fn new() -> PatternTrie<'a, 'b> {
+        PatternTrie{ root: Node::new() }
+    }
+
+    pub fn insert(&mut self, pattern: CompiledPattern) -> Result<&str, &str> {
+        for i in pattern.into_iter() {
+            match i {
+                NodeType::Literal(literal) => {
+                    let pos = self.root.literal_children.find_pos(&literal);
+                },
+                NodeType::Parser(boxed_parser) => {
+
+                }
+            }
+        }
+
+
+        Err("err")
+    }
 }
