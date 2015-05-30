a9329bd053aa5a03fa813b96a867bad236a06650
Tibor Benke <ihrwein@gmail.com>
2015-05-30 16:09:44 +0200

    LiteralNode: compare only their first characters in cmp()
    When two LiteralNodes begin with the same character, they should go
    under the same parent Node.

--- src/matcher/trie/node/literal.rs
+++ src/matcher/trie/node/literal.rs
@@ -13,34 +13,66 @@ impl <'a, 'b> LiteralNode<'a, 'b> {
         LiteralNode{ literal: literal,
                      node: None}
     }
+
+    fn compare_first_chars(&self, other : &LiteralNode) -> Ordering {
+        if self.literal.is_empty() && other.literal.is_empty() {
+            Ordering::Equal
+        } else if self.literal.is_empty() {
+            Ordering::Less
+        } else if other.literal.is_empty() {
+            Ordering::Greater
+        } else {
+            self.literal[0..1].cmp(&other.literal[0..1])
+        }
+    }
 }
 
 impl <'a, 'b> Eq for LiteralNode<'a, 'b> {}
 
 impl <'a, 'b> PartialEq for LiteralNode<'a, 'b> {
     fn eq(&self, other: &Self) -> bool {
-        self.literal == other.literal
+        self.compare_first_chars(other) == Ordering::Equal
     }
 
     fn ne(&self, other: &Self) -> bool {
-        self.literal != other.literal
+        self.compare_first_chars(other) != Ordering::Equal
     }
 }
 
 impl <'a, 'b> Ord for LiteralNode<'a, 'b> {
     fn cmp(&self, other: &Self) -> Ordering {
-        self.literal.cmp(&other.literal)
+        self.compare_first_chars(other)
     }
 }
 
 impl <'a, 'b> PartialOrd for LiteralNode<'a, 'b> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.literal.cmp(&other.literal))
+        Some(self.compare_first_chars(other))
     }
 }
 
 impl <'a, 'b> Borrow<str> for LiteralNode<'a, 'b> {
     fn borrow(&self) -> &str {
         self.literal
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use matcher::trie::node::LiteralNode;
+    use std::cmp::Ordering;
+
+    #[test]
+    fn given_literal_node_when_it_is_compared_to_an_other_literal_node_then_only_their_first_chars_are_checked() {
+        let alpha = LiteralNode::new("alpha");
+        let beta = LiteralNode::new("beta");
+        let aleph = LiteralNode::new("aleph");
+        let empty = LiteralNode::new("");
+
+        assert_eq!(alpha.cmp(&beta), Ordering::Less);
+        assert_eq!(alpha.cmp(&aleph), Ordering::Equal);
+        assert_eq!(beta.cmp(&alpha), Ordering::Greater);
+        assert_eq!(alpha.cmp(&empty), Ordering::Greater);
+        assert_eq!(empty.cmp(&alpha), Ordering::Less);
     }
 }
