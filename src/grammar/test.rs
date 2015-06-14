642f8cfad13ca54d9b511661ced5dcfb2dd39666
Tibor Benke <ihrwein@gmail.com>
2015-06-14 23:42:56 +0200

    grammar: work on the grammar to parse the pattern correctly

--- src/grammar/test.rs
+++ src/grammar/test.rs
@@ -1,35 +1,83 @@
 use super::pattern_parser;
 use matcher::trie::node::{CompiledPattern, NodeType};
 
-#[test]
-fn test_given_parser_as_a_string_when_it_is_parsed_then_we_get_the_instantiated_parser() {
-    let string_parser = "%{INT:test_name}";
-    match pattern_parser::part_parser(string_parser).ok().unwrap() {
-        NodeType::Parser(parser) => {
-            assert_eq!(parser.base().name(), "test_name");
-        },
-        _ => unreachable!()
+fn assert_parser_name_equals<'a>(item: Option<&NodeType<'a>>, expected_name: &str) {
+    if let Some(&NodeType::Parser(ref parser)) = item {
+        assert_eq!(parser.base().name(), expected_name);
+    } else {
+        unreachable!();
+    }
+}
+
+fn assert_literal_equals<'a>(item: Option<&NodeType<'a>>, expected: &str) {
+    if let Some(&NodeType::Literal(literal)) = item {
+        assert_eq!(literal, expected);
+    } else {
+        unreachable!();
     }
 }
 
 #[test]
+fn test_given_parser_as_a_string_when_it_is_parsed_then_we_get_the_instantiated_parser() {
+    let string_parser = "%{INT:test_name}";
+    let mut vec = pattern_parser::pattern(string_parser).ok().unwrap();
+
+    assert_eq!(vec.len(), 1);
+    println!("{:?}", &vec);
+    assert_parser_name_equals(vec.get(0), "test_name");
+}
+
+#[test]
 fn test_given_parser_as_a_string_when_its_name_is_invalid_then_we_dont_get_the_instantiated_parser() {
-    pattern_parser::part_parser("%{INT:test$name}").err().unwrap();
-    pattern_parser::part_parser("%{INT:test-name}").err().unwrap();
-    pattern_parser::part_parser("%{INT:-").err().unwrap();
-    pattern_parser::part_parser("%{INT:").err().unwrap();
+    pattern_parser::pattern("%{INT:test$name}").err().unwrap();
+    pattern_parser::pattern("%{INT:test-name}").err().unwrap();
+    pattern_parser::pattern("%{INT:-").err().unwrap();
+    pattern_parser::pattern("%{INT:").err().unwrap();
 }
 
 #[test]
 fn test_given_parser_as_a_string_when_its_name_is_valid_then_we_get_the_instantiated_parser() {
-    pattern_parser::part_parser("%{INT:test_name}").ok().unwrap();
-    pattern_parser::part_parser("%{INT:test}").ok().unwrap();
-    pattern_parser::part_parser("%{INT:TEST_NAME_}").ok().unwrap();
-    pattern_parser::part_parser("%{INT:_}").ok().unwrap();
+    pattern_parser::pattern("%{INT:test_name}").ok().unwrap();
+    pattern_parser::pattern("%{INT:test}").ok().unwrap();
+    pattern_parser::pattern("%{INT:TEST_NAME_}").ok().unwrap();
+    pattern_parser::pattern("%{INT:_}").ok().unwrap();
 }
 
 #[test]
 fn test_given_parser_as_a_string_when_its_type_isnt_exist_then_we_get_an_error() {
     let string_parser_with_invalid_type = "%{INVALID:test_name}";
-    pattern_parser::part_parser(string_parser_with_invalid_type).err().unwrap();
+    pattern_parser::pattern(string_parser_with_invalid_type).err().unwrap();
+}
+
+
+#[test]
+fn test_given_literal_as_a_string_when_it_is_parsed_then_we_stop_at_the_parsers_begin() {
+    let expected = "foo ";
+    let mut vec = pattern_parser::pattern(expected).ok().unwrap();
+
+    assert_eq!(vec.len(), 1);
+
+    assert_literal_equals(vec.get(0), expected);
+}
+
+#[test]
+fn test_given_pattern_as_a_string_when_it_is_parsed_with_the_grammar_we_got_the_right_compiled_pattern() {
+    let pattern_as_string = "foo %{INT:int_0} bar %{INT:int_1}%{INT:int_2} baz";
+    let mut vec: Vec<NodeType<>> = pattern_parser::pattern(pattern_as_string).ok().unwrap();
+
+    assert_eq!(vec.len(), 6);
+    assert_literal_equals(vec.get(0), "foo ");
+    assert_parser_name_equals(vec.get(1), "int_0");
+    assert_literal_equals(vec.get(2), " bar ");
+    assert_parser_name_equals(vec.get(3), "int_1");
+    assert_parser_name_equals(vec.get(4), "int_2");
+    assert_literal_equals(vec.get(5), " baz");
+}
+
+#[test]
+#[should_panic]
+fn test_given_invalid_string_when_we_parse_it_then_the_parser_returns_with_error() {
+    let pattern_as_string = "foo %{INT:int_0 baz";
+    let _ = pattern_parser::pattern(pattern_as_string).ok().unwrap();
+
 }
