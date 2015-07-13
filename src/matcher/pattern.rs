77de0b7c821e7d71bc0fa60ecb0f2fd20f5f5f33
Tibor Benke <ihrwein@gmail.com>
2015-07-13 19:55:46 +0200

    Pattern: fix Yaml deserialization error
    When the name field wasn't present in the Yaml document we failed to
    deserialize.
    
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/matcher/pattern.rs
+++ src/matcher/pattern.rs
@@ -60,7 +60,6 @@ impl Pattern {
     }
 
     pub fn from_yaml(doc: &Yaml) -> Result<Pattern, FromYamlError> {
-        println!("doc: {:?}", doc);
         let mut pattern = None;
         let mut name = None;
         let mut uuid = None;
@@ -68,12 +67,10 @@ impl Pattern {
             pattern = hash.get(&Yaml::String("pattern".to_string()));
             name = hash.get(&Yaml::String("name".to_string()));
             uuid = hash.get(&Yaml::String("uuid".to_string()));
-            println!("name: {:?} uuid: {:?} pattern {:?}", name, uuid, pattern);
         }
 
         let name_final = {
-            let unwrapped = try!(name.ok_or(FromYamlError::SchemaError));
-            unwrapped.as_str().map(|x| x.to_string())
+            name.and_then(|x| x.as_str()).map(|x| x.to_string())
         };
 
         let uuid_final = {
@@ -107,7 +104,7 @@ impl Pattern {
 #[cfg(test)]
 mod test {
     use super::Pattern;
-    use yaml_rust::{Yaml, YamlLoader};
+    use yaml_rust::YamlLoader;
     use uuid::Uuid;
 
     #[test]
@@ -126,5 +123,19 @@ pattern: "Jun %{INT:day} %{INT:hour}:%{INT:min}:%{INT:sec} lobotomy sshd[%{INT:p
         assert_eq!(pattern.name(), Some("SSH_DISCONNECT"));
         assert_eq!(pattern.uuid().as_bytes(), expected_uuid.as_bytes());
         assert_eq!(pattern.pattern().len(), 15);
+    }
+
+    #[test]
+    fn test_given_yaml_pattern_when_it_does_not_have_name_then_pattern_can_be_built_from_it() {
+        let buffer = r#"
+uuid: 9a49c47d-29e9-4072-be84-3b76c6814743
+pattern: "Jun %{INT:day} %{INT:hour}:%{INT:min}:%{INT:sec} lobotomy sshd[%{INT:pid}]: Received disconnect from %{GREEDY:ipaddr}: %{INT:dunno}: disconnected by user"
+        "#;
+
+        let docs = YamlLoader::load_from_str(buffer).unwrap();
+        assert_eq!(docs.len(), 1);
+        let doc = &docs[0];
+        let pattern = Pattern::from_yaml(doc).ok().unwrap();
+        assert_eq!(pattern.name(), None);
     }
 }
