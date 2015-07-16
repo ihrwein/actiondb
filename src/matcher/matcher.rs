a772f1277c6f963203094c4094b9dc16e97e0180
Tibor Benke <ihrwein@gmail.com>
2015-07-16 18:59:16 +0200

    Matcher: check TestMessages when a Matcher is built from a JSON document
    Here is the JSON loading process:
    1. deserialize the file into a pattern:file::File object
    2. extract the Vec<Pattern> from the File
    3. extract the TestMessages from each Pattern
    4. insert the Patterns into the ParserTrie
    5. check every TestMessage
     * when it's message is parsed we got the expected key-value pairs
    
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/matcher/matcher.rs
+++ src/matcher/matcher.rs
@@ -1,14 +1,16 @@
 use uuid::Uuid;
 
 use std::fs;
+use std::borrow::Borrow;
 use std::io::{BufReader, BufRead};
 use grammar::parser;
 use grammar::parser::ParseError;
 use super::trie::ParserTrie;
 use super::result::MatchResult;
-use super::errors::BuildFromFileError;
+use super::errors::{BuildFromFileError, FromJsonError};
 use super::pattern::Pattern;
 use super::pattern::file;
+use super::pattern::testmessage::TestMessage;
 
 #[derive(Clone, Debug)]
 pub struct Matcher {
@@ -22,7 +24,7 @@ impl Matcher {
         Ok(Matcher{ parser: trie })
     }
 
-    pub fn from_json_file(pattern_file_path: &str) -> Result<Matcher, super::pattern::file::Error> {
+    pub fn from_json_file(pattern_file_path: &str) -> Result<Matcher, FromJsonError> {
         let file = try!(file::File::open(pattern_file_path));
         let trie = try!(Matcher::build_trie_from_json_file(file));
         Ok(Matcher{ parser: trie })
@@ -48,14 +50,49 @@ impl Matcher {
         Ok(trie)
     }
 
-    fn build_trie_from_json_file(file: file::File) -> Result<ParserTrie, super::pattern::file::Error> {
+    fn build_trie_from_json_file(file: file::File) -> Result<ParserTrie, FromJsonError> {
         let mut trie = ParserTrie::new();
-        let file::File {patterns} = file;
+        let file::File {mut patterns} = file;
 
+        let test_messages = Matcher::extract_test_messages_from_patterns(&mut patterns);
         for pattern in patterns.into_iter() {
             trie.insert(pattern);
         }
 
+        try!(Matcher::check_test_messages_on_trie(&trie, &test_messages));
+
         Ok(trie)
+    }
+
+    fn extract_test_messages_from_patterns(patterns: &mut Vec<Pattern>) -> Vec<TestMessage> {
+        let mut test_messages = Vec::new();
+
+        for mut pattern in patterns {
+            while let Some(test_message) = pattern.pop_test_message() {
+                test_messages.push(test_message);
+            }
+        }
+
+        test_messages
+    }
+
+    fn check_test_messages_on_trie(trie: &ParserTrie, messages: &[TestMessage]) -> Result<(), FromJsonError> {
+        for msg in messages {
+            if let Some(result) = trie.parse(msg.message()) {
+
+                if result.pairs().len() != msg.values().len() {
+                    return Err(FromJsonError::TestMessage);
+                }
+                for &(k, v) in result.pairs() {
+                    let expected_value = msg.values().get(k);
+                    if expected_value.map(|x| x.borrow()) != Some(v) {
+                        return Err(FromJsonError::TestMessage);
+                    }
+                }
+            } else {
+                return Err(FromJsonError::TestMessage);
+            }
+        }
+        Ok(())
     }
 }
