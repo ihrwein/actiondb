05a91e36c4b2e4e061442951937e3ce1ba88a7d9
Tibor Benke <tibor.benke@balabit.com>
2015-07-17 17:12:51 +0200

    BuildError: add support for converting plain::Error to BuildError
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/matcher/matcher/builder/error.rs
+++ src/matcher/matcher/builder/error.rs
@@ -1,9 +1,11 @@
 use matcher::errors::FromJsonError;
 use matcher::pattern::file::serialized;
+use matcher::pattern::file::plain;
 
 #[derive(Debug)]
 pub enum BuildError {
-    FromJson(FromJsonError)
+    FromJson(FromJsonError),
+    FromPlain(plain::Error)
 }
 
 impl From<FromJsonError> for BuildError {
@@ -15,5 +17,11 @@ impl From<FromJsonError> for BuildError {
 impl From<serialized::Error> for BuildError {
     fn from(error: serialized::Error) -> BuildError {
         BuildError::FromJson(FromJsonError::from(error))
+    }
+}
+
+impl From<plain::Error> for BuildError {
+    fn from(error: plain::Error) -> BuildError {
+        BuildError::FromPlain(error)
     }
 }
