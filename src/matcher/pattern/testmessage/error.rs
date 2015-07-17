e877430ac4716ea9fb686f3c6b6c67438f0b1313
Tibor Benke <tibor.benke@balabit.com>
2015-07-17 10:34:06 +0200

    TestPairsError: implement Debug trait
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/matcher/pattern/testmessage/error.rs
+++ src/matcher/pattern/testmessage/error.rs
@@ -1,3 +1,6 @@
+use std::error;
+use std::fmt;
+
 #[derive(Debug)]
 pub enum TestPairsError {
     InvalidLength{expected: usize, got: usize},
@@ -18,3 +21,20 @@ impl TestPairsError {
         TestPairsError::KeyNotFound{key: key.to_string()}
     }
 }
+
+impl fmt::Display for TestPairsError {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self {
+            &TestPairsError::InvalidLength{expected, got} => {
+                fmt.write_fmt(format_args!("The number of parsed key-value pairs does not equal to their expected number: expected={} got={}", expected, got))
+            },
+            &TestPairsError::ValueNotMatch{ref key, ref expected_value, ref got_value} => {
+                fmt.write_fmt(format_args!("A parsed value does not equal to its expected value: key={} expected={} got={}", key, expected_value, got_value))
+            },
+            &TestPairsError::KeyNotFound{ref key} => {
+                fmt.write_fmt(format_args!("A parsed key in not found among the expected ones: key={}", key))
+            }
+        }
+    }
+}
+
