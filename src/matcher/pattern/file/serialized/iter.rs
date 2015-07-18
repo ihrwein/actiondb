644f3ce200c2a90fd5bba8c1140d47e1ad6b7c97
Tibor Benke <ihrwein@gmail.com>
2015-07-18 22:07:44 +0200

    SerializedPatternFile: adapt the new BuildResult iteration method
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/matcher/pattern/file/serialized/iter.rs
+++ src/matcher/pattern/file/serialized/iter.rs
@@ -1,14 +1,29 @@
 use super::SerializedPatternFile;
 use matcher::pattern::Pattern;
+use matcher::pattern::source::BuildResult;
 
 use std::iter;
-use std::vec;
 
 impl iter::IntoIterator for SerializedPatternFile {
-    type Item = Pattern;
-    type IntoIter = vec::IntoIter<Self::Item>;
+    type Item = BuildResult;
+    type IntoIter = IntoIter;
 
     fn into_iter(self) -> Self::IntoIter {
-        self.patterns.into_iter()
+        IntoIter{patterns: self.patterns}
+    }
+}
+
+pub struct IntoIter {
+    patterns: Vec<Pattern>
+}
+
+impl Iterator for IntoIter {
+    type Item = BuildResult;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.patterns.pop() {
+            Some(pattern) => Some(Ok(pattern)),
+            None => None
+        }
     }
 }
