edbfedc0b261c592454f987af34b054046a9823d
Tibor Benke <ihrwein@gmail.com>
2015-07-19 16:38:27 +0200

    matcher::Builder: use the Matcher trait
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/matcher/matcher/builder/builder.rs
+++ src/matcher/matcher/builder/builder.rs
@@ -1,24 +1,22 @@
-use matcher::trie::ParserTrie;
 use matcher::pattern::{Pattern, PatternSource};
 use matcher::pattern::testmessage::{TestMessage, TestPairsError};
-
+use matcher::Matcher;
 use super::BuildError;
 
 pub struct Builder;
 
 impl Builder {
-    pub fn drain_into(from: &mut PatternSource, to: &mut ParserTrie) -> Result<(), BuildError>{
+    pub fn drain_into(from: &mut PatternSource, matcher: &mut Matcher) -> Result<(), BuildError>{
         for pattern in from {
             let mut pattern = try!(pattern);
-            let test_messages = Builder::extract_test_messages_from_pattern(&mut pattern);
-            to.insert(pattern);
-            try!(Builder::check_test_messages_on_trie(&to, &test_messages));
+            let test_messages = Builder::extract_test_messages(&mut pattern);
+            matcher.add_pattern(pattern);
+            try!(Builder::check_test_messages(matcher, &test_messages));
         }
-
         Ok(())
     }
 
-    fn extract_test_messages_from_pattern(pattern: &mut Pattern) -> Vec<TestMessage> {
+    fn extract_test_messages(pattern: &mut Pattern) -> Vec<TestMessage> {
         let mut messages = Vec::new();
 
         while let Some(test_message) = pattern.pop_test_message() {
@@ -27,9 +25,9 @@ impl Builder {
         messages
     }
 
-    fn check_test_messages_on_trie(trie: &ParserTrie, messages: &[TestMessage]) -> Result<(), BuildError> {
+    fn check_test_messages(matcher: &Matcher, messages: &[TestMessage]) -> Result<(), BuildError> {
         for msg in messages {
-            let result = try!(trie.parse(msg.message()).ok_or(TestPairsError::TestMessageDoesntMatch));
+            let result = try!(matcher.parse(msg.message()).ok_or(TestPairsError::TestMessageDoesntMatch));
             try!(msg.test_pairs(result.pairs()));
         }
         Ok(())
