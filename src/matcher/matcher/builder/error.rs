0bd24114ba7e150fc5a331478458b72d62cb963f
Tibor Benke <ihrwein@gmail.com>
2015-07-19 15:08:36 +0200

    BuilderError: use serialized::Error instead of FromJsonError
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/matcher/matcher/builder/error.rs
+++ src/matcher/matcher/builder/error.rs
@@ -1,30 +1,29 @@
-use matcher::errors::FromJsonError;
 use matcher::pattern::file::serialized;
 use matcher::pattern::file::plain;
 use matcher::pattern::testmessage;
 
 #[derive(Debug)]
 pub enum BuildError {
-    FromSerialized(FromJsonError),
+    FromSerialized(serialized::Error),
     FromPlain(plain::Error),
     TestPairs(testmessage::TestPairsError),
     TestMessageDoesntMatch
 }
 
-impl From<FromJsonError> for BuildError {
-    fn from(error: FromJsonError) -> BuildError {
-        BuildError::FromSerialized(error)
-    }
-}
-
 impl From<serialized::Error> for BuildError {
     fn from(error: serialized::Error) -> BuildError {
-        BuildError::FromSerialized(FromJsonError::from(error))
+        BuildError::FromSerialized(error)
     }
 }
 
 impl From<plain::Error> for BuildError {
     fn from(error: plain::Error) -> BuildError {
         BuildError::FromPlain(error)
+    }
+}
+
+impl From<testmessage::TestPairsError> for BuildError {
+    fn from(error: testmessage::TestPairsError) -> BuildError {
+        BuildError::TestPairs(error)
     }
 }
