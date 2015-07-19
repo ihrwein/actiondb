1b820e48eb9f218a7cae8626efe14bf9df02db34
Tibor Benke <ihrwein@gmail.com>
2015-07-19 16:35:42 +0200

    trie: implement Matcher for ParserTrie
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

---
+++ src/matcher/trie/matcher.rs
@@ -0,0 +1,13 @@
+use matcher::Matcher;
+use super::ParserTrie;
+use matcher::result::MatchResult;
+use matcher::pattern::Pattern;
+
+impl Matcher for ParserTrie {
+    fn parse<'a, 'b>(&'a self, text: &'b str) -> Option<MatchResult<'a, 'b>> {
+        self.parse(text)
+    }
+    fn add_pattern(&mut self, pattern: Pattern) {
+        self.insert(pattern);
+    }
+}
