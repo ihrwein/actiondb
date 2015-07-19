b22a671e5e40b9065b2043e21587045684b4c390
Tibor Benke <ihrwein@gmail.com>
2015-07-19 15:50:26 +0200

    matcher/factory: rename Matcher to Factory
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/matcher/factory.rs
+++ src/matcher/factory.rs
@@ -5,28 +5,24 @@ use super::pattern::source::PatternSource;
 use super::matcher::builder;
 
 #[derive(Clone, Debug)]
-pub struct Matcher {
+pub struct Factory {
     parser: ParserTrie
 }
 
-impl Matcher {
-    pub fn from_file(pattern_file_path: &str) -> Result<Matcher, builder::BuildError> {
+impl Factory {
+    pub fn from_plain_file(pattern_file_path: &str) -> Result<Factory, builder::BuildError> {
         let file = try!(file::PlainPatternFile::open(pattern_file_path));
-        Matcher::drain_into(&mut file.into_iter())
+        Factory::drain_into(&mut file.into_iter())
     }
 
-    pub fn from_json_file(pattern_file_path: &str) -> Result<Matcher, builder::BuildError> {
+    pub fn from_json_file(pattern_file_path: &str) -> Result<Factory, builder::BuildError> {
         let file = try!(file::SerializedPatternFile::open(pattern_file_path));
-        Matcher::drain_into(&mut file.into_iter())
+        Factory::drain_into(&mut file.into_iter())
     }
 
-    pub fn drain_into(source: &mut PatternSource) -> Result<Matcher, builder::BuildError> {
+    pub fn drain_into(source: &mut PatternSource) -> Result<Factory, builder::BuildError> {
         let mut trie = ParserTrie::new();
         try!(builder::Builder::drain_into(source, &mut trie));
-        Ok(Matcher{ parser: trie })
-    }
-
-    pub fn parse<'a, 'b>(&'a self, text: &'b str) -> Option<MatchResult<'a, 'b>> {
-        self.parser.parse(text)
+        Ok(Factory{ parser: trie })
     }
 }
