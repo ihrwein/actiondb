72f516267236866f21c27ee42e5e6bc64f246e8e
Tibor Benke <ihrwein@gmail.com>
2015-07-22 20:03:20 +0200

    matcher/pattern/file/plain/error: implement fmt::Display trait
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/matcher/pattern/file/plain/error.rs
+++ src/matcher/pattern/file/plain/error.rs
@@ -1,5 +1,6 @@
 use grammar::parser::ParseError;
 use std::io;
+use std::fmt;
 
 #[derive(Debug)]
 pub enum Error {
@@ -16,5 +17,14 @@ impl From<io::Error> for Error {
 impl From<ParseError> for Error {
     fn from(error: ParseError) -> Error {
         Error::PatternParse(error)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self {
+            &Error::PatternParse(ref error) => error.fmt(formatter),
+            &Error::IO(ref error) => error.fmt(formatter)
+        }
     }
 }
