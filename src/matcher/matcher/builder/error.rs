4157e5f0283e221d17bff49d549968d225fe4b22
Tibor Benke <tibor.benke@balabit.com>
2015-07-23 10:33:32 +0200

    BuildError: implement fmt::Display trait
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/matcher/matcher/builder/error.rs
+++ src/matcher/matcher/builder/error.rs
@@ -2,6 +2,8 @@ use matcher::pattern::file::serialized;
 use matcher::pattern::file::plain;
 use matcher::pattern::testmessage;
 
+use std::fmt;
+
 #[derive(Debug)]
 pub enum BuildError {
     FromSerialized(serialized::Error),
@@ -26,5 +28,17 @@ impl From<plain::Error> for BuildError {
 impl From<testmessage::Error> for BuildError {
     fn from(error: testmessage::Error) -> BuildError {
         BuildError::TestMessage(error)
+    }
+}
+
+impl fmt::Display for BuildError {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self {
+            &BuildError::FromSerialized(ref error) => error.fmt(formatter),
+            &BuildError::FromPlain(ref error) => error.fmt(formatter),
+            &BuildError::TestMessage(ref error) => error.fmt(formatter),
+            &BuildError::UnsupportedFileExtension => formatter.write_fmt(format_args!("The given file extension is not suppoted")),
+            &BuildError::NotUtf8FileName => formatter.write_str("The given filename contains non Utf-8 characters"),
+        }
     }
 }
