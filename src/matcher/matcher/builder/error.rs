dde5bcc9bf221779fee6eac0bdd08bc46793f717
Tibor Benke <tibor.benke@balabit.com>
2015-07-23 10:40:23 +0200

    BuildError: implement error::Error trait
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/matcher/matcher/builder/error.rs
+++ src/matcher/matcher/builder/error.rs
@@ -3,6 +3,7 @@ use matcher::pattern::file::plain;
 use matcher::pattern::testmessage;
 
 use std::fmt;
+use std::error;
 
 #[derive(Debug)]
 pub enum BuildError {
@@ -39,6 +40,28 @@ impl fmt::Display for BuildError {
             &BuildError::TestMessage(ref error) => error.fmt(formatter),
             &BuildError::UnsupportedFileExtension => formatter.write_fmt(format_args!("The given file extension is not suppoted")),
             &BuildError::NotUtf8FileName => formatter.write_str("The given filename contains non Utf-8 characters"),
+        }
+    }
+}
+
+impl error::Error for BuildError {
+    fn description(&self) -> &str {
+        match self {
+            &BuildError::FromSerialized(ref error) => error.description(),
+            &BuildError::FromPlain(ref error) => error.description(),
+            &BuildError::TestMessage(ref error) => error.description(),
+            &BuildError::UnsupportedFileExtension => "The given file extension is not supported",
+            &BuildError::NotUtf8FileName => "The given filename contains non Utf-8 characters"
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match self {
+            &BuildError::FromSerialized(ref error) => error.cause(),
+            &BuildError::FromPlain(ref error) => error.cause(),
+            &BuildError::TestMessage(ref error) => error.cause(),
+            &BuildError::UnsupportedFileExtension => None,
+            &BuildError::NotUtf8FileName => None
         }
     }
 }
