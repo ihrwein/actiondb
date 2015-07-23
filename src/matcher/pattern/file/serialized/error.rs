671053dffde7158ed8fa21b52802a08e861a3f1b
Tibor Benke <tibor.benke@balabit.com>
2015-07-23 09:30:38 +0200

    matcher/pattern/file/serialized/error: implement error::Error for DeserError
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/matcher/pattern/file/serialized/error.rs
+++ src/matcher/pattern/file/serialized/error.rs
@@ -1,27 +1,16 @@
 use std::io;
-use std::fmt;
-use serde::json;
+
+pub use self::deser::DeserError;
 
 #[derive(Debug)]
 pub enum Error {
     IO(io::Error),
-    Deser(DeserError)
-}
-
-#[derive(Debug)]
-pub enum DeserError {
-    JSON(json::Error)
+    Deser(deser::DeserError)
 }
 
 impl From<io::Error> for Error {
     fn from(error: io::Error) -> Error {
         Error::IO(error)
-    }
-}
-
-impl From<json::Error> for DeserError {
-    fn from(error: json::Error) -> DeserError {
-        DeserError::JSON(error)
     }
 }
 
@@ -31,10 +20,41 @@ impl From<DeserError> for Error {
     }
 }
 
-impl fmt::Display for DeserError {
-    fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        match self {
-            &DeserError::JSON(ref error) => error.fmt(formatter)
+mod deser {
+    use std::fmt;
+    use std::error;
+    use serde::json;
+
+    #[derive(Debug)]
+    pub enum DeserError {
+        JSON(json::Error)
+    }
+
+    impl From<json::Error> for DeserError {
+        fn from(error: json::Error) -> DeserError {
+            DeserError::JSON(error)
+        }
+    }
+
+    impl fmt::Display for DeserError {
+        fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+            match self {
+                &DeserError::JSON(ref error) => error.fmt(formatter)
+            }
+        }
+    }
+
+    impl error::Error for DeserError {
+        fn description(&self) -> &str {
+            match self {
+                &DeserError::JSON(ref error) => error.description()
+            }
+        }
+
+        fn cause(&self) -> Option<&error::Error> {
+            match self {
+                &DeserError::JSON(ref error) => error.cause()
+            }
         }
     }
 }
