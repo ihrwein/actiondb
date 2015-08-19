ae9637d53d605db7c3fdd1c80eca015f6270e119
Tibor Benke <tibor.benke@balabit.com>
2015-08-19 16:43:54 +0200

    patterm/file: remove PlainPatternFile
    It clashed with the idea of having CR characters in patterns.
    
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/matcher/matcher/builder/error.rs
+++ src/matcher/matcher/builder/error.rs
@@ -1,5 +1,4 @@
 use matcher::pattern::file::serialized;
-use matcher::pattern::file::plain;
 use matcher::pattern::testmessage;
 
 use std::fmt;
@@ -8,7 +7,6 @@ use std::error;
 #[derive(Debug)]
 pub enum BuildError {
     FromSerialized(serialized::Error),
-    FromPlain(plain::Error),
     TestMessage(testmessage::Error),
     UnsupportedFileExtension,
     NotUtf8FileName
@@ -17,12 +15,6 @@ pub enum BuildError {
 impl From<serialized::Error> for BuildError {
     fn from(error: serialized::Error) -> BuildError {
         BuildError::FromSerialized(error)
-    }
-}
-
-impl From<plain::Error> for BuildError {
-    fn from(error: plain::Error) -> BuildError {
-        BuildError::FromPlain(error)
     }
 }
 
@@ -36,7 +28,6 @@ impl fmt::Display for BuildError {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         match self {
             &BuildError::FromSerialized(ref error) => error.fmt(formatter),
-            &BuildError::FromPlain(ref error) => error.fmt(formatter),
             &BuildError::TestMessage(ref error) => error.fmt(formatter),
             &BuildError::UnsupportedFileExtension => formatter.write_fmt(format_args!("The given file extension is not suppoted")),
             &BuildError::NotUtf8FileName => formatter.write_str("The given filename contains non Utf-8 characters"),
@@ -48,7 +39,6 @@ impl error::Error for BuildError {
     fn description(&self) -> &str {
         match self {
             &BuildError::FromSerialized(ref error) => error.description(),
-            &BuildError::FromPlain(ref error) => error.description(),
             &BuildError::TestMessage(ref error) => error.description(),
             &BuildError::UnsupportedFileExtension => "The given file extension is not supported",
             &BuildError::NotUtf8FileName => "The given filename contains non Utf-8 characters"
@@ -58,7 +48,6 @@ impl error::Error for BuildError {
     fn cause(&self) -> Option<&error::Error> {
         match self {
             &BuildError::FromSerialized(ref error) => error.cause(),
-            &BuildError::FromPlain(ref error) => error.cause(),
             &BuildError::TestMessage(ref error) => error.cause(),
             &BuildError::UnsupportedFileExtension => None,
             &BuildError::NotUtf8FileName => None
--- src/matcher/pattern/file/plain/mod.rs
+++
@@ -1,6 +0,0 @@
-mod error;
-mod iter;
-mod file;
-
-pub use self::error::Error;
-pub use self::file::PlainPatternFile;
