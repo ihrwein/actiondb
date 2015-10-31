59ab4f644bfcfce60f20166470bacec77541cce8
Tibor Benke <ihrwein@gmail.com>
2015-10-31 10:34:01 +0100

    BuildError: match on *self
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/matcher/matcher/builder/error.rs
+++ src/matcher/matcher/builder/error.rs
@@ -26,12 +26,12 @@ impl From<testmessage::Error> for BuildError {
 
 impl fmt::Display for BuildError {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        match self {
-            &BuildError::FromSerialized(ref error) => error.fmt(formatter),
-            &BuildError::TestMessage(ref error) => error.fmt(formatter),
-            &BuildError::UnsupportedFileExtension =>
+        match *self {
+            BuildError::FromSerialized(ref error) => error.fmt(formatter),
+            BuildError::TestMessage(ref error) => error.fmt(formatter),
+            BuildError::UnsupportedFileExtension =>
                 formatter.write_fmt(format_args!("The given file extension is not suppoted")),
-            &BuildError::NotUtf8FileName =>
+            BuildError::NotUtf8FileName =>
                 formatter.write_str("The given filename contains non Utf-8 characters"),
         }
     }
@@ -39,20 +39,20 @@ impl fmt::Display for BuildError {
 
 impl error::Error for BuildError {
     fn description(&self) -> &str {
-        match self {
-            &BuildError::FromSerialized(ref error) => error.description(),
-            &BuildError::TestMessage(ref error) => error.description(),
-            &BuildError::UnsupportedFileExtension => "The given file extension is not supported",
-            &BuildError::NotUtf8FileName => "The given filename contains non Utf-8 characters",
+        match *self {
+            BuildError::FromSerialized(ref error) => error.description(),
+            BuildError::TestMessage(ref error) => error.description(),
+            BuildError::UnsupportedFileExtension => "The given file extension is not supported",
+            BuildError::NotUtf8FileName => "The given filename contains non Utf-8 characters",
         }
     }
 
     fn cause(&self) -> Option<&error::Error> {
-        match self {
-            &BuildError::FromSerialized(ref error) => error.cause(),
-            &BuildError::TestMessage(ref error) => error.cause(),
-            &BuildError::UnsupportedFileExtension => None,
-            &BuildError::NotUtf8FileName => None,
+        match *self {
+            BuildError::FromSerialized(ref error) => error.cause(),
+            BuildError::TestMessage(ref error) => error.cause(),
+            BuildError::UnsupportedFileExtension => None,
+            BuildError::NotUtf8FileName => None,
         }
     }
 }
