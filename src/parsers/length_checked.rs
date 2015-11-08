80b66647bf23d0b55e8b8a91921d4014611e0685
Tibor Benke <ihrwein@gmail.com>
2015-11-08 11:52:25 +0100

    parsers: test HasLengthConstraint trait
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/parsers/length_checked.rs
+++ src/parsers/length_checked.rs
@@ -62,21 +62,49 @@ impl LengthCheckedParserBase {
 
 #[cfg(test)]
 mod test {
-    use super::LengthCheckedParserBase;
+    use parsers::HasLengthConstraint;
+
+    struct DummyImpl {
+        min_length: Option<usize>,
+        max_length: Option<usize>,
+    }
+
+    impl DummyImpl {
+        fn new() -> DummyImpl {
+            DummyImpl {
+                min_length: None,
+                max_length: None,
+            }
+        }
+    }
+
+    impl HasLengthConstraint for DummyImpl {
+        fn min_length(&self) -> Option<usize> {
+            self.min_length
+        }
+        fn set_min_length(&mut self, length: Option<usize>) {
+            self.min_length = length;
+        }
+        fn max_length(&self) -> Option<usize> {
+            self.max_length
+        }
+        fn set_max_length(&mut self, length: Option<usize>) {
+            self.max_length = length;
+        }
+    }
 
     #[test]
     fn test_given_parser_when_the_match_length_is_not_constrained_then_the_match_length_is_ok_in_every_case
         () {
-        let base = LengthCheckedParserBase::with_name("name".to_string());
+        let base = DummyImpl::new();
         assert_eq!(base.is_match_length_ok(42), true);
         assert_eq!(base.is_match_length_ok(1), true);
     }
 
     #[test]
-    fn test_given_parser_when_the_minimum_match_length_is_set_then_the_shorter_matches_are_discarded
-        () {
-        let mut base = LengthCheckedParserBase::with_name("name".to_string());
-        base.set_min_length(10);
+    fn test_given_parser_when_the_minimum_match_length_is_set_then_the_shorter_matches_are_discarded() {
+        let mut base = DummyImpl::new();
+        base.set_min_length(Some(10));
         assert_eq!(base.is_match_length_ok(42), true);
         assert_eq!(base.is_match_length_ok(1), false);
         assert_eq!(base.is_match_length_ok(9), false);
@@ -86,8 +114,8 @@ mod test {
     #[test]
     fn test_given_parser_when_the_maximum_match_length_is_set_then_the_longer_matches_are_discarded
         () {
-        let mut base = LengthCheckedParserBase::with_name("name".to_string());
-        base.set_max_length(10);
+        let mut base = DummyImpl::new();
+        base.set_max_length(Some(10));
         assert_eq!(base.is_match_length_ok(42), false);
         assert_eq!(base.is_match_length_ok(1), true);
         assert_eq!(base.is_match_length_ok(9), true);
