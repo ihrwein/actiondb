cb8efc64a12cc2e01a3f15786ab7b2310ef057ad
Tibor Benke <ihrwein@gmail.com>
2016-01-20 21:45:14 +0100

    matcher/suffix_array/test: rename a function
    create_complex_parser_trie() -> create_populated_suffix_table()
    
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/matcher/suffix_array/test.rs
+++ src/matcher/suffix_array/test.rs
@@ -6,7 +6,7 @@ use parsers::SetParser;
 use matcher::pattern::Pattern;
 use matcher::Matcher;
 
-fn create_complex_parser_trie() -> SuffixTable {
+fn create_populated_suffix_table() -> SuffixTable {
     let mut root = SuffixTable::new();
     let cp1 = CompiledPatternBuilder::new()
                 .literal("app")
@@ -46,7 +46,7 @@ fn create_complex_parser_trie() -> SuffixTable {
 
 #[test]
 fn test_given_parser_trie_when_a_parser_is_not_matched_then_the_parser_stack_is_unwind_so_an_untried_parser_is_tried() {
-    let root = create_complex_parser_trie();
+    let root = create_populated_suffix_table();
     println!("root: {:?}", &root);
     {
         let result = root.parse("app42letree123");
