318be437adaafe99b6ffc4639ac484a1e8ce3e62
Tibor Benke <ihrwein@gmail.com>
2016-01-20 21:02:00 +0100

    matcher/suffix_array/test: add unit test for parser hashing
    Signed-off-by: Tibor Benke <ihrwein@gmail.com>

--- src/matcher/suffix_array/test.rs
+++ src/matcher/suffix_array/test.rs
@@ -1,5 +1,6 @@
 use super::impls::SuffixTable;
 use super::interface::{SuffixArray, LiteralEntry};
+use parsers::IntParser;
 use matcher::compiled_pattern::CompiledPatternBuilder;
 use parsers::SetParser;
 use matcher::pattern::Pattern;
@@ -44,7 +45,6 @@ fn create_complex_parser_trie() -> SuffixTable {
 }
 
 #[test]
-#[no_mangle]
 fn test_given_parser_trie_when_a_parser_is_not_matched_then_the_parser_stack_is_unwind_so_an_untried_parser_is_tried() {
     let root = create_complex_parser_trie();
     println!("root: {:?}", &root);
@@ -52,6 +52,27 @@ fn test_given_parser_trie_when_a_parser_is_not_matched_then_the_parser_stack_is_
         let result = root.parse("app42letree123");
         assert_eq!(result.expect("Failed to get result").values(), &btreemap!["end" => "123", "middle" => "42"]);
     }
+}
+
+#[test]
+fn test_given_suffix_array_when_a_parser_entry_is_inserted_it_is_only_added_if_it_is_a_new_parser() {
+    let mut root = SuffixTable::new();
+    let cp1 = CompiledPatternBuilder::new()
+                .parser(Box::new(IntParser::new()))
+                .build();
+    let cp2 = CompiledPatternBuilder::new()
+                .parser(Box::new(IntParser::new()))
+                .build();
+
+    let mut pattern1 = Pattern::with_random_uuid();
+    pattern1.set_pattern(cp1);
+    root.insert(pattern1);
+
+    let mut pattern2 = Pattern::with_random_uuid();
+    pattern2.set_pattern(cp2);
+    root.insert(pattern2);
+
+    assert_eq!(true, root.parse("42").is_some());
 }
 
 #[test]
