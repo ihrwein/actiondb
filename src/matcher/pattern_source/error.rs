7f01f994fd4c366f385d66165ea920b9cd9d73ca
Tibor Benke <tibor.benke@balabit.com>
2016-03-24 16:23:35 +0100

    BuildError: add DeserYaml variant
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/matcher/pattern_source/error.rs
+++ src/matcher/pattern_source/error.rs
@@ -1,6 +1,7 @@
 use matcher::pattern::testmessage;
 
 use serde_json;
+use serde_yaml;
 use std::fmt;
 use std::error;
 use std::io;
@@ -10,6 +11,7 @@ pub enum BuildError {
     TestMessage(testmessage::Error),
     Io(io::Error),
     DeserJson(serde_json::Error),
+    DeserYaml(serde_yaml::Error),
     UnsupportedFileExtension,
     NotUtf8FileName,
 }
@@ -32,12 +34,19 @@ impl From<serde_json::Error> for BuildError {
     }
 }
 
+impl From<serde_yaml::Error> for BuildError {
+    fn from(error: serde_yaml::Error) -> BuildError {
+        BuildError::DeserYaml(error)
+    }
+}
+
 impl fmt::Display for BuildError {
     fn fmt(&self, formatter: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         match *self {
             BuildError::TestMessage(ref error) => error.fmt(formatter),
             BuildError::Io(ref error) => error.fmt(formatter),
             BuildError::DeserJson(ref error) => error.fmt(formatter),
+            BuildError::DeserYaml(ref error) => error.fmt(formatter),
             BuildError::UnsupportedFileExtension =>
                 formatter.write_fmt(format_args!("The given file extension is not suppoted")),
             BuildError::NotUtf8FileName =>
@@ -52,6 +61,7 @@ impl error::Error for BuildError {
             BuildError::TestMessage(ref error) => error.description(),
             BuildError::Io(ref error) => error.description(),
             BuildError::DeserJson(ref error) => error.description(),
+            BuildError::DeserYaml(ref error) => error.description(),
             BuildError::UnsupportedFileExtension => "The given file extension is not supported",
             BuildError::NotUtf8FileName => "The given filename contains non Utf-8 characters",
         }
@@ -62,6 +72,7 @@ impl error::Error for BuildError {
             BuildError::TestMessage(ref error) => error.cause(),
             BuildError::Io(ref error) => error.cause(),
             BuildError::DeserJson(ref error) => error.cause(),
+            BuildError::DeserYaml(ref error) => error.cause(),
             BuildError::UnsupportedFileExtension | BuildError::NotUtf8FileName => None,
         }
     }
